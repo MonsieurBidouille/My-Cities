c4eff114041c471ebe1fd681c3ba3c0e367115f2
Julien <julien.epenoy@uha.fr>
2023-06-08 14:29:32 +0200

    Changement pending

---
+++ screen/modifbuildings.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import WhiteButton from '../components/white_button';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  TextInput
+} from 'react-native';
+import {connect} from "react-redux";
+
+import * as SQLite from 'expo-sqlite';
+
+
+class Modifbuilding extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+           
+          }
+}
+
+componentDidMount(){
+    
+    
+    let formdata = new FormData;
+
+    fetch('http://jdevalik.fr/api/mycities/.php', {
+                method: 'POST', 
+                body: formdata, 
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                },})
+                .then((response) => response.json())
+                .then((json) => {
+                    if(json != false){
+
+                    }else{
+                       return false;
+                    }
+                })
+                .catch((error) => {
+                        console.error(error);
+                    }
+                );
+}
+
+
+valid(){
+  const {navigate} = this.props.navigation;
+  const {crnt_id} = this.props;
+  let formdata = new FormData;
+
+  fetch('http://jdevalik.fr/api/mycities/updateuser.php', {
+    method: 'POST', 
+    body: formdata, 
+    headers: {
+        "Content-Type": "multipart/form-data"
+    },})
+    .then((response) => response.json())
+    .then((json) => {
+        if(json != false){
+          const action1 = {type:"crnt_user",value:this.state.email};
+          this.props.dispatch(action1);
+          navigate("valid");
+        }else{
+          Alert.alert("Veuillez entrer des informations valides.");
+        }
+    })
+    .catch((error) => {
+            console.error(error);
+        }
+    );
+
+}
+
+render(){
+        
+    return (
+        <View style={styless.container}>
+                   <TextInput style={styles.input} value={this.state.name} onChangeText={text=> this.setState({name:text})}  placeholder="Nom" keyboardType="text"/>
+                   <TextInput style={styles.input} value={this.state.fname} onChangeText={text=> this.setState({fname:text})}  placeholder="Prénom" keyboardType="text"/>
+                   <TextInput style={styles.input} value={this.state.email} onChangeText={text=> this.setState({email:text})}  placeholder="Email" keyboardType="text"/>
+                   <TextInput style={styles.input} value={this.state.pass} onChangeText={text=> this.setState({pass:text})}  placeholder="Password" keyboardType="text"/>
+                   <TextInput style={styles.input} value={this.state.tel} onChangeText={text=> this.setState({tel:text})}  placeholder="Téléphone" keyboardType="text"/>
+                   <TextInput style={styles.input} value={this.state.civ} onChangeText={text=> this.setState({civ:text})}  placeholder="Civilité" keyboardType="text"/>
+                   <WhiteButton style={{height: 20}} val = "Valider"  onPress={() => this.valid()}></WhiteButton>
+        </View>
+    )
+}
+
+}
+
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      width: 220,
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor:'white',
+      margin: 10
+    }
+    ,
+  });const styless = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  
+  });
+
+const mapStateToProps = (state)=>{
+    return state;}
+export default connect(mapStateToProps)(Modify);
